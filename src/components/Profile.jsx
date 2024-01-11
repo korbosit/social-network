9b629bdcd865de0de49f3c6b88211b27ef06d2b1
Vlaimir Korba <krissky1986@gmail.com>
2024-01-11 13:23:00 +0200

    lesson12 decomposition into components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2tErgFZ2UXuDEfIQjOYHsw2QE0mJRE7xY57VvnquBnA&s" />
+            </div>
+            <div>ava + descr</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
