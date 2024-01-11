9ad61ca2c1a4e80a5476f00f40175bdc4a5ff575
Vlaimir Korba <krissky1986@gmail.com>
2024-01-11 22:28:11 +0200

    20-lesson NavLink

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,49 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
             <div className={`${classes.item} ${classes.active}`}>
-                <a href="/profile">Profile</a>
+                <NavLink
+                    className={(navData) =>
+                        navData.isActive ? classes.active : classes.item
+                    }
+                    to="/profile"
+                >
+                    Profile
+                </NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/dialogs">Messages</a>
+                <NavLink
+                    className={(navData) =>
+                        navData.isActive ? classes.active : classes.item
+                    }
+                    to="/dialogs"
+                >
+                    Messages
+                </NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/news">News</a>
+                <NavLink
+                    className={(navData) =>
+                        navData.isActive ? classes.active : classes.item
+                    }
+                    to="/news"
+                >
+                    News
+                </NavLink>
             </div>
             <div className={classes.item}>
-                <a href="/music">Music</a>
+                <NavLink
+                    className={(navData) =>
+                        navData.isActive ? classes.active : classes.item
+                    }
+                    to="/music"
+                >
+                    Music
+                </NavLink>
             </div>
         </nav>
     );
