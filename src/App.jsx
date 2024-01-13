dcecc57bb8cacc53c862b360f0c19bf795278f37
Vlaimir Korba <krissky1986@gmail.com>
2024-01-13 14:37:47 +0200

    22-lesson Route exact

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
                 <div className="app-wrapper-content">
                     <Routes>
                         <Route path="/profile" element={<Profile />} />
-                        <Route path="/dialogs" element={<Dialogs />} />
+                        <Route path="/dialogs/*" element={<Dialogs />} />
                     </Routes>
                 </div>
             </div>
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,20 +1,35 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <div className={classes.dialog}>Vovanec</div>
-                <div className={classes.dialog}>Svetlana</div>
-                <div className={`${classes.dialog} ${classes.active}`}>
-                    Timka
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/1">Vovanec</NavLink>
                 </div>
-                <div className={classes.dialog}>Beta</div>
-                <div className={classes.dialog}>Alfa</div>
-                <div className={classes.dialog}>Mikki</div>
-                <div className={classes.dialog}>Madlena</div>
-                <div className={classes.dialog}>Alica</div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/2">Svetlana</NavLink>
+                </div>
+                <div className={`${classes.dialog} ${classes.active}`}>
+                    <NavLink to="/dialogs/3">Timka</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/4">Beta</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/5">Alfa</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/6">Mikki</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/7">Madlena</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to="/dialogs/8">Alica</NavLink>
+                </div>
             </div>
             <div className={classes.messages}>
                 <div className={classes.message}>Hi</div>
