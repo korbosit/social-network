c4b2aaaacebbc93720b24238af18992804badc31
Vlaimir Korba <krissky1986@gmail.com>
2024-01-13 15:58:39 +0200

    24-lesson UI-BLL put the data aside

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,22 +16,119 @@ const Message = (props) => {
 };
 
 const Dialogs = () => {
+    let dialogsData = [
+        {
+            id: 1,
+            name: "Vovanec",
+        },
+        {
+            id: 2,
+            name: "Svetlana",
+        },
+        {
+            id: 3,
+            name: "Timka",
+        },
+        {
+            id: 4,
+            name: "Beta",
+        },
+        {
+            id: 5,
+            name: "Alfa",
+        },
+        {
+            id: 6,
+            name: "Mikki",
+        },
+        {
+            id: 7,
+            name: "Madlena",
+        },
+        {
+            id: 8,
+            name: "Alica",
+        },
+    ];
+    let messagesData = [
+        {
+            id: 1,
+            message: "Hi",
+        },
+        {
+            id: 2,
+            message: "Hello",
+        },
+        {
+            id: 3,
+            message: "How are you?",
+        },
+        {
+            id: 4,
+            message: "Yo-yo-yo",
+        },
+        {
+            id: 5,
+            message: "I will back",
+        },
+        {
+            id: 6,
+            message: "Astalavista Baby",
+        },
+        {
+            id: 7,
+            name: "How old are you",
+        },
+        {
+            id: 8,
+            message: "What is your favorite book?",
+        },
+    ];
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name="Vovanec" id="1" />
-                <DialogItem name="Svetlana" id="2" />
-                <DialogItem name="Timka" id="3" />
-                <DialogItem name="Beta" id="4" />
-                <DialogItem name="Alfa" id="5" />
-                <DialogItem name="Mikki" id="6" />
-                <DialogItem name="Madlena" id="7" />
-                <DialogItem name="Alica" id="8" />
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
+                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id} />
+                <DialogItem name={dialogsData[6].name} id={dialogsData[6].id} />
+                <DialogItem name={dialogsData[7].name} id={dialogsData[7].id} />
             </div>
             <div className={classes.messages}>
-                <Message message="Hello" />
-                <Message message="How are you?" />
-                <Message message="Yo-yo" />
+                <Message
+                    message={messagesData[0].message}
+                    id={messagesData[0].id}
+                />
+                <Message
+                    message={messagesData[1].message}
+                    id={messagesData[1].id}
+                />
+                <Message
+                    message={messagesData[2].message}
+                    id={messagesData[2].id}
+                />
+                <Message
+                    message={messagesData[3].message}
+                    id={messagesData[3].id}
+                />
+                <Message
+                    message={messagesData[4].message}
+                    id={messagesData[4].id}
+                />
+                <Message
+                    message={messagesData[5].message}
+                    id={messagesData[5].id}
+                />
+                <Message
+                    message={messagesData[6].message}
+                    id={messagesData[6].id}
+                />
+                <Message
+                    message={messagesData[7].message}
+                    id={messagesData[7].id}
+                />
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,48 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+    let postData = [
+        {
+            id: 1,
+            message: "Hi 1111",
+            likesCount: 12,
+        },
+        {
+            id: 2,
+            message: "Hello 22222",
+            likesCount: 123,
+        },
+        {
+            id: 3,
+            message: "How are you? 33333333",
+            likesCount: 34,
+        },
+        {
+            id: 4,
+            message: "Yo-yo-yo 444444444",
+            likesCount: 48,
+        },
+        {
+            id: 5,
+            message: "I will back 55555555",
+            likesCount: 64,
+        },
+        {
+            id: 6,
+            message: "Astalavista Baby 66666",
+            likesCount: 17,
+        },
+        {
+            id: 7,
+            name: "How old are you 777777777",
+            likesCount: 99,
+        },
+        {
+            id: 8,
+            message: "What is your favorite book? 8888",
+            likesCount: 127,
+        },
+    ];
     return (
         <div className={classes.postsBlock}>
             My posts
@@ -16,8 +58,46 @@ const MyPosts = () => {
             </div>
             <div>New post</div>
             <div className={classes.posts}>
-                <Post message="Hi, how are you?" />
-                <Post message="Its my first post" />
+                <Post
+                    message={postData[0].message}
+                    likesCount={postData[0].id}
+                    id={postData[0].id}
+                />
+                <Post
+                    message={postData[1].message}
+                    likesCount={postData[1].id}
+                    id={postData[1].id}
+                />
+                <Post
+                    message={postData[2].message}
+                    likesCount={postData[2].id}
+                    id={postData[2].id}
+                />
+                <Post
+                    message={postData[3].message}
+                    likesCount={postData[3].id}
+                    id={postData[3].id}
+                />
+                <Post
+                    message={postData[4].message}
+                    likesCount={postData[4].id}
+                    id={postData[4].id}
+                />
+                <Post
+                    message={postData[5].message}
+                    likesCount={postData[5].id}
+                    id={postData[5].id}
+                />
+                <Post
+                    message={postData[6].message}
+                    likesCount={postData[6].id}
+                    id={postData[6].id}
+                />
+                <Post
+                    message={postData[7].message}
+                    likesCount={postData[7].id}
+                    id={postData[7].id}
+                />
             </div>
         </div>
     );
