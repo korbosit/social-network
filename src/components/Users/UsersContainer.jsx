af85d1d067fe61138f5d2edeb988bead2e8f51a8
Vlaimir Korba <krissky1986@gmail.com>
2024-01-20 18:53:32 +0200

    56-lesson Presentation and Container components

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -8,6 +8,43 @@ import {
     setTotalUsersCountAC,
 } from "../../redux/users-reducer";
 import Users from "./Users";
+import instance from "../../redux/api-instance";
+class UsersContainer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidMount() {
+        instance
+            .get(
+                `/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+            )
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        instance
+            .get(`/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then((response) => {
+                this.props.setUsers(response.data.items);
+            });
+    };
+    render() {
+        return (
+            <Users
+                totalUsersCount={this.props.totalUsersCount}
+                pageSize={this.props.pageSize}
+                currentPage={this.props.currentPage}
+                onPageChanged={this.onPageChanged}
+                users={this.props.users}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        );
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -40,4 +77,4 @@ let mapDispatchToProps = (dispatch) => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
