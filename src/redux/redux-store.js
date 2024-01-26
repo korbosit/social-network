83f2a383df721a84815a31cf64b6f02f4eb8b186
Vlaimir Korba <krissky1986@gmail.com>
2024-01-26 16:28:58 +0200

    75-lesson redux-form basis-1

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -9,13 +9,16 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import { thunk as thunkMiddleware } from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 // import { thunk } from "redux-thunk";
+
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
